e057f68b58fabcf1e0a3ca615dc21090a46abf7c
alincode <alincode@gmail.com>
2018-01-13 12:26:14 +0800

    refactory

---
+++ main.js
@@ -0,0 +1,31 @@
+var _ = require('lodash')
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    infos: []
+  },
+  methods: {
+    init: function (event) {
+      var url = 'https://alincode.github.io/digital-nomad-guide-to-taiwan/data.json';
+      // var url = 'http://127.0.0.1:8080/data.json';
+      fetch(url).then(function (response) {
+        return response.json()
+      }).then(function (jsonData) {
+        app.infos = jsonData;
+      }).catch(function (err) { });
+    },
+    switchContent: function (event) {
+      var id = event.target.id;
+      // console.log(id);
+      // tab
+      $('.list-group .active').removeClass('active');
+      $(this).addClass('active');
+
+      // content
+      $('.tab-pane').hide();
+      $('[aria-labelledby=' + id + ']').show();
+    }
+  }
+});
+app.init();
